a4026fe0e2a8a2daa03648a80a85df367034cb5c
Rodrigo Costa <costa.rgc@gmail.com>
2019-08-24 12:53:47 -0400

    upsy2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from 'react';
 import './assets/css/App.css';
+import HeaderMain from './components/HeaderMain';
+import TechUsed from './components/TechUsed';
 
 function App() {
   return (
 
-    <main className="container">
+    <main className="app">
 
-      <div className="col-12" style={{ textAlign: "center" }}>
-
-        <div>
-          <img src={require('./assets/images/logo_upself.svg')} width="350" alt="logo" />
-        </div>
-
-        <div>
-          Hey I'm Upsy! I'm here the make your day better!
-        </div>
-
-      </div>
-
-
-
-
-      <div className="col-12" style={{ textAlign: "center" }}>
-        <div style={{ border: "200px"}} >
-          <img id="applink" style={{ opacity: 1, margin: "5px" }} src={require('./assets/images/app_store_badge.png')} height="40" alt="appstore" />
-          <img id="applink" style={{ opacity: 1, margin: "5px" }} src={require('./assets/images/google_play_badge.png')} height="40" alt="googleplay" />
-        </div>
-      </div>
-
-
-
+      <HeaderMain />    
+      <TechUsed />
 
     </main>
-
-
-
-
 
   );
 }
---
+++ src/components/TechUsed.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/css/App.css';
+
+function TechUsed() {
+    return (
+
+        <div className="col-12"
+            style={{
+                textAlign: "center",
+                paddingTop: "75px",
+                paddingBottom: "75px",
+                backgroundColor: "white"
+            }}>
+
+            <div>
+                <h1> Technology we use</h1>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default TechUsed;
