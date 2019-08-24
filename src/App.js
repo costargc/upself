27e0ba78b612788ca1255723683fd9dcc49e2c16
Rodrigo Costa <costa.rgc@gmail.com>
2019-08-23 23:05:49 -0400

    first upsy

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './assets/css/App.css';
 function App() {
   return (
 
-    <main className="page js-page">
+    <main className="container">
 
-      <img src={require('./assets/images/logo_upself.svg')} height="250" alt="logo"/>
+      <div className="col-12" style={{ textAlign: "center" }}>
 
-      <div>
-        Everybody could use someone like me
+        <div>
+          <img src={require('./assets/images/logo_upself.svg')} height="250" alt="logo" />
+        </div>
+
+        <div>
+          Everybody could use someone like me
+        </div>
+
       </div>
 
-
-      <img id="applink" style={{ opacity: 1 }} src={require('./assets/images/app_store_badge.png')} height="50" alt="appstore"/>
-      <img id="applink" style={{ opacity: 1 }} src={require('./assets/images/google_play_badge.png')} height="50" alt="googleplay"/>
-
+      <div className="col-12" style={{ textAlign: "center" }}>
+        <img id="applink" style={{ opacity: 1 }} src={require('./assets/images/app_store_badge.png')} height="50" alt="appstore" />
+        <img id="applink" style={{ opacity: 1 }} src={require('./assets/images/google_play_badge.png')} height="50" alt="googleplay" />
+      </div>
 
     </main>
 
