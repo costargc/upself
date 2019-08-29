b05122c613dd8365270a9f97a591025169153c04
Matt Gilliland <mgilliland23@gmail.com>
2019-08-29 19:00:03 -0400

    added natural lang understanding to chat

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -9,6 +9,13 @@ import {
 import "react-chat-widget/lib/styles.css";
 import logo from "../../assets/images/check/check1.svg";
 
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1.js");
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: "2019-07-12",
+  iam_apikey: "4qwcu7_ydMn0lH6Sb0lxlLOLMVxc44NK4UNVgjfM6tsV",
+  url: "https://gateway.watsonplatform.net/natural-language-understanding/api"
+});
+
 class Chat extends Component {
   componentDidMount() {
     addResponseMessage("Hi! How are you doing today?");
@@ -16,6 +23,26 @@ class Chat extends Component {
 
   handleNewUserMessage = newMessage => {
     console.log(`New message incoming! ${newMessage}`);
+
+    const analyzeParams = {
+      html:
+        "<html><head><title>Fruits</title></head><body><h1>Apples and Oranges</h1><p>I love apples! I don't like oranges.</p></body></html>",
+      features: {
+        emotion: {
+          targets: ["apples", "oranges"]
+        }
+      }
+    };
+
+    naturalLanguageUnderstanding
+      .analyze(analyzeParams)
+      .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+      })
+      .catch(err => {
+        console.log("error:", err);
+      });
+
     addResponseMessage("I'm still learning!");
     // Now send the message throught the backend API
   };
