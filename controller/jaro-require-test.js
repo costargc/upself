c76d9e26b2f6e9d42ac1f329119769317574c591
Rodrigo Costa <costa.rgc@gmail.com>
2019-09-04 15:58:24 -0400

    remove test files

--- controller/jaro-require-test.js
+++
@@ -1,4 +0,0 @@
-JaroWinkler = require('./jaro-winkler');
-
-console.log('Q1. '+JaroWinkler.jaroWinkler_CS("who r u?",'what is your name?',));
-console.log('Q1. '+"who r u?".jaroWinkler('what is your name?'));
