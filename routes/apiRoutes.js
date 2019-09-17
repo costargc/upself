38a8c23605fb6e58b312a7c20db67d0ebdfce7d4
Rodrigo Costa <costa.rgc@gmail.com>
2019-09-17 15:21:02 -0400

    backend start

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,22 @@ let AnalyzeMessage = require("../controller/AnalyzeMessage.js");
 let ResponseBuild = require("../controller/ResponseBuild.js");
 // var ChatbotDataset = JSON.parse(fs.readFileSync('./controller/data/english/trivia.json'));
 
-
 const router = require("express").Router();
 
 // Post route for receiving user's message and returning IBM Watson's Analysis
 
 
 
-router.post("/analyzeMessage", function (req, res) {
-  console.log(req.body.params);
+// router.post("/analyzeMessage", function (req, res) {
+//   console.log(req.body.params);
 
-  new AnalyzeMessage(req.body.params.userMessage).emotion.then(function (
-    result
-  ) {
-    console.log("Emotion results: ", result);
-    res.json(result);
-  });
-});
+//   new AnalyzeMessage(req.body.params.userMessage).emotion.then(function (
+//     result
+//   ) {
+//     console.log("Emotion results: ", result);
+//     res.json(result);
+//   });
+// });
 
 
 
