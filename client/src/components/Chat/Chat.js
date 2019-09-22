18467e6e64442f0fdfac30a7a08287ee886dc0f6
emilybaum <emily.r.baum@gmail.com>
2019-09-22 01:05:15 -0400

    added and removed comments as needed

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -51,29 +51,24 @@ class Chat extends Component {
     // white msg as a user - to be used after a button is clicked
     // addUserMessage("this is a test as a user");
 
-    // renderCustomComponent(<Chat_img link="../../assets/images/upsy_friend.svg"/>);
-    // renderCustomComponent(Chat_img,{link: require("../../assets/images/upsy_help.svg")},true);
 
   }
  
+  // Used for single responses in the chat
   displayMessgae = response => {
+
+    // shows the typing dots
     renderCustomComponent(Typing, {}, true)
+
     // delaying the reply messgae to be 1 second (in the hops of having the Typing component display for that amount of time)
     setTimeout(() => {
-
       return addResponseMessage(`${response}`)
 
     }, 1000)
-    
   }
 
 
   handleNewUserMessage = newMessage => {
-  
-    // this.setState({isTyping: true})
-    
-    // this will pass the component Typing in, but we need that component to destroy itself... not sure how to do that
-    // renderCustomComponent(Typing, {}, true)
 
     API.getMessageJaro(`${newMessage}`).then((response) => {
       console.log(`${response}`)
@@ -81,37 +76,28 @@ class Chat extends Component {
       // check if the selected response is not a string (aka it's an array)
       const response1 = response
       if (typeof response1 !== "string" ) {
-        
-        // loop through the array and append the message on the page
-        // for (var i = 0; i < response1.length; i++) {
-        //   renderCustomComponent(Typing, {}, true)
-        //   // this.displayMessgae(`${response1[i]}`)
-        //   // addResponseMessage(`${response1[i]}`)
 
-        //     setTimeout(() => {
-        //       addResponseMessage(`${response}`)
-        //     }, 1000)            
-        // }
+        // this function will display the dots then the response, then more dots in sync
         (async function loop() {
           for (let i = 0; i < response1.length; i++) {
 
+            // waits for the whole setTimeout to complete before moving to other indexes in the response array
             await new Promise(resolve => {
               renderCustomComponent(Typing, {}, true)
               setTimeout(resolve, 1000)
 
             });
 
+            // shows the typing dots
             addResponseMessage(`${response1[i]}`)
             console.log("for loop: " + i);
-            
+
           }
         })();
       }
 
       else {
-        console.log("waiting....")
         this.displayMessgae(`${response}`)
-        // addResponseMessage(`${response}`)
       }
 
     });
